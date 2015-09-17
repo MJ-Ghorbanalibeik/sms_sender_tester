14291ad15200765251f75e8693b9eb4822a59017
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2015-09-17 13:56:08 +0430

    Use rails logger for debug

--- lib/sms_sender_tester.rb
+++ lib/sms_sender_tester.rb
@@ -1,6 +1,6 @@
 module SmsSenderTester
   def self.send_sms(credentials, mobile_number, message, sender, options = nil)
-    debug('SmsSenderTester::send_sms method. ' + 
+    Rails.logger.debug('SmsSenderTester::send_sms method. ' + 
       'Credentials: ' + credentials.to_s + 
       'Mobile number: ' + mobile_number.to_s + 
       'Message: ' + message + 
@@ -10,13 +10,13 @@ module SmsSenderTester
   end
 
   def self.get_balance(credentials)
-    debug('SmsSenderTester::get_balance method. ' + 
+    Rails.logger.debug('SmsSenderTester::get_balance method. ' + 
       'Credentials: ' + credentials.to_s
     )
   end
 
   def self.query_message(credentials, msgid)
-    debug('SmsSenderTester::query_message method. ' + 
+    Rails.logger.debug('SmsSenderTester::query_message method. ' + 
       'Credentials: ' + credentials.to_s + 
       'Message id: ' + msgid.to_s 
     )
