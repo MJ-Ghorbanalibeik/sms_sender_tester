f1680876538e2137b1f36dbfbf612610b2212e0b
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2015-09-17 15:54:02 +0430

    Add options to raise error or exception

--- lib/sms_sender_tester.rb
+++ lib/sms_sender_tester.rb
@@ -1,5 +1,7 @@
 module SmsSenderTester
   def self.send_sms(credentials, mobile_number, message, sender, options = nil)
+    return {error: 'Raise error for some reason!'} if options && options[:raise_error] = true
+    raise 'Raise exception for some reason!' if options && options[:raise_exception] = true
     Rails.logger.debug('SmsSenderTester::send_sms method. ' + 
       'Credentials: ' + credentials.to_s + 
       'Mobile number: ' + mobile_number.to_s + 
