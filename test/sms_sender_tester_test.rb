41b0edfe89edac605e57b9a255b74450b3958883
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2015-09-17 13:58:36 +0430

    Add method call test

--- test/sms_sender_tester_test.rb
+++ test/sms_sender_tester_test.rb
@@ -3,5 +3,11 @@ require 'test_helper'
 class SmsSenderTesterTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, SmsSenderTester
+    SmsSenderTester.send_sms(
+      {username: 'test username', password: 'test password'},
+      'test mobile_number',
+      'test message',
+      'test sender'
+    )
   end
 end
